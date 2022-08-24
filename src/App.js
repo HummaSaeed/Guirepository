16e766f021377ced9518cbb6ec33fa5288527f86
HummaSaeed <84771295+HummaSaeed@users.noreply.github.com>
2022-08-24 11:53:59 +0500

    App Design

--- src/App.js
+++ src/App.js
@@ -2,8 +2,38 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <p> Weather App</p>
+    <div className="app">
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>Dellas</p>
+          </div>
+          <div className="temp">
+            <h1>
+              60 <span>&#176;</span>F
+            </h1>
+          </div>
+          <div className="description">
+            <p>Clouds</p>
+          </div>
+        </div>
+        <div className="bottom">
+          <div className="feel">
+            <p className="bold">
+              65<span>&#176;</span>F
+            </p>
+            <p>Feels Like</p>
+          </div>
+          <div className="humidity">
+            <p className="bold">20%</p>
+            <p>Humidity</p>
+          </div>
+          <div className="wind">
+            <p className="bold">12 MPH</p>
+            <p>Wind Speed</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
